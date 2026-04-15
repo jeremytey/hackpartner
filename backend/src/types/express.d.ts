a905820fcc8cf68a97cb88a42e63fb23c2c7332c
Jeremy Tey <jeremy8598@gmail.com>
2026-04-15 11:30:25 +0800

    fixed(auth): tsconfig file and express.d.ts fixed

--- backend/src/types/express.d.ts
+++ backend/src/types/express.d.ts
@@ -1,11 +1,11 @@
-import { JwtPayload } from "./auth.types";
-
 // extends the Express Request interface to include a user property, 
 // which will hold the decoded JWT payload after authentication.
 declare global {
     namespace Express {
         interface Request {
-            user?: JwtPayload;
+            user?: import('./auth.types').JwtPayload;
         }
     }
 }
+
+export {};
